47cdbe91dcb4f6469f588b39a99c5008721f4df7
Priyanka.Vishe <vishepriyanka0@gmail.com>
2025-07-20 21:42:17 +0530

    dynamic data added in signup api

--- src/app.js
+++ src/app.js
@@ -4,34 +4,17 @@ const {connectDb} = require('../config/database');
 const app = express();
 const User = require('../models/user');
 
+// <-----------without express.json req.body undiefined yenar------------->
+app.use(express.json());
+// as postman or from req we are sending json data but server can not read json data , hence to read we need middleware  i.e express.json()
 app.post('/signup', async (req,res) => {
-    const userObj = {
-        firstName: "Shivani",
-        lastName: "Vishe",
-        emailId: "qwe@gmail.com",
-        password: "qwe123"
-    }
-    //to save this obj in our mongodb, so need to create instance of User model
-    const user = new User(userObj);
-
-    // or can add directly
-    // const user = new User({
-    //     firstName: "Priyanka",
-    //     lastName: "Vishe",
-    //     emailId: "xyz@gmail.com",
-    //     password: "abc123"
-    // })
-
-    // <-------------all db operatiosn return promises hence wrap them inside async await ---------------->
-    // await user.save();
-    // res.send("User added successfully");
-
-    //<---------------wrap all db operations await in try catch------------->
+    //console.log(req.body);
+    const user = new User(req.body);
     try{
         await user.save();
-        res.send("User added successfully");
+        res.send("User data saved successfully");
     }catch(err){
-        res.status(500).send("Error saving the user");
+        res.status(500).send("failed saving user data");
     }
 })
 
