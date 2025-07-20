1a326ec989bb3dc50ce63e45c5dfc7b478c281cd
Priyanka.Vishe <vishepriyanka0@gmail.com>
2025-07-20 16:06:04 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -8,12 +8,48 @@ const app = express();
 // app.use((req,res)=>{
 //     res.send("hey started");
 // });  
+
+// app.use('/',(req,res)=>{
+//     res.send("hey started");
+// });  
 // _________________________
 
-
+//will match all http methods post ,get
 app.use('/test',(req,res)=>{
     res.send("tested");
 });
+
+// app.use('/hello',(req,res)=>{
+//     res.send("two");
+// });
+
+app.get('/user',(req,res)=>{
+    //res.send("user data");
+    res.send({"name" : "priyanka", "age": 23});
+});
+app.post('/user',(req,res)=>{
+    res.send("posting user data");
+    //res.send({"name" : "priyanka", "age": 23});
+});
+
+app.delete('/user',(req,res)=>{
+    res.send("deleting user data");
+    //res.send({"name" : "priyanka", "age": 23});
+});
+
+app.put('/user',(req,res)=>{
+    res.send("using put for user data");
+    //res.send({"name" : "priyanka", "age": 23});
+});
+// app.use('//hello',(req,res)=>{
+//     res.send("two");
+// }); ///brobr yet 
+
+// even if /hello/2 la two print hoil
+// app.use('/hello/2',(req,res)=>{
+//     res.send("hey");
+// });
+
 
 app.listen(7777, ()=>{
     console.log("started server");
